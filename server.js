e1473396fd7eecf31aa9372eb1107a6598cf2446
Glitch (grape-anthropology) <none>
2019-11-06 13:15:50 +0000

    👙🐡 Checkpoint ./package.json:7955134/7003 ./server.js:7955134/91

--- server.js
+++ server.js
@@ -4,10 +4,13 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 
 const mongoose = require('mongoose')
-mongoose.connect(process.env.MLAB_URI || 'mongodb://localhost/exercise-track', { useMongoClient: true } )
-  .catch(error => console.error(error))
+mongoose.connect(process.env.MLAB_URI || 'mongodb://localhost/exercise-track', { 
+  useNewUrlParser: true, 
+  useUnifiedTopology: true 
+})
+  .catch(error => console.error(error));
 
-app.use(cors())
+app.use(cors());
 
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
